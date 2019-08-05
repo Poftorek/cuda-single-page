c553b07bc39856b007900a01f16c89fb32507675
Poftorek <fyeem@o2.pl>
2019-08-05 20:05:07 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -58,6 +60,13 @@ module.exports = {
       hash: true,
       template: './src/index.html',
       filename: 'index.html'
+    }),
+    new CopyWebpackPlugin([{
+      from: './src/assets/',
+      to: 'assets/'
+    }]),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i
     })
   ]
 };
