388b5d7350b724641a1eb4d5e7e570641b087aec
Poftorek <Poftorek@o2.pl>
2019-08-05 17:05:59 +0200

    Add header component, break styles on few stylesheets, add assets folder

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 import './style.scss'
 
+const navItems = document.querySelectorAll('.nav__item');
+for (const item of navItems) {
+  item.addEventListener('click', function () {
+    for (const item of navItems) {
+      item.classList.remove('nav__item--active');
+    }
+    this.classList.add('nav__item--active');
+  });
+}
