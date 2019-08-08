3cc444590d99d78e8eb419a832e5075a7b816b9c
Poftorek <Poftorek@o2.pl>
2019-08-08 15:18:01 +0200

    Add portfolio section in RWD and correct main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import './style.scss'
 
-const navItems = document.querySelectorAll('.nav__item');
-for (const item of navItems) {
-  item.addEventListener('click', function () {
-    for (const item of navItems) {
-      item.classList.remove('nav__item--active');
-    }
-    this.classList.add('nav__item--active');
-  });
+const handleActiveButtons = (name, activeClass) => {
+  const Items = document.querySelectorAll(name);
+  for (const item of Items) {
+    item.addEventListener('click', function () {
+      for (const item of Items) {
+        item.classList.remove(activeClass);
+      }
+      this.classList.add(activeClass);
+    });
+  }
 }
+handleActiveButtons('.nav__item', 'nav__item--active');
+handleActiveButtons('.buttons-group__btn', 'buttons-group__btn--active');
